721dac0d8f0d69a3438a38694dd355729c3de436
VitaliyPliss <v.pliss@mail.ru>
2022-02-11 15:20:10 +0300

    задание 3 готово. Реализованы маршруты

--- src/frontend/src/layouts/AppLayout.vue
+++ src/frontend/src/layouts/AppLayout.vue
@@ -1,31 +1,18 @@
 <template>
-  <header class="header">
-    <div class="header__logo">
-      <a href="index.html" class="logo">
-        <img
-          src="~@/assets/img/logo.svg"
-          alt="V!U!E! Pizza logo"
-          width="90"
-          height="40"
-        />
-      </a>
-    </div>
-    <div class="header__cart">
-      <a href="cart.html">{{ orderPrice }} ₽</a>
-    </div>
-    <div class="header__user">
-      <a href="#" class="header__login"><span>Войти</span></a>
-    </div>
-  </header>
+  <div class="app-layout">
+    <component :is="layout">
+      <slot />
+    </component>
+  </div>
 </template>
 
 <script>
 export default {
   name: "AppLayout",
-  props: {
-    orderPrice: {
-      type: Number,
-      required: true,
+  computed: {
+    layout() {
+      const layout = this.$route.meta?.layout || "AppLayoutDefault";
+      return () => import(`@/layouts/${layout}.vue`);
     },
   },
 };
