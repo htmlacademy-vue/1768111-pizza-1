fd454e5beccc032f2e46de9c1562115cffd176b4
VitaliyPliss <v.pliss@mail.ru>
2022-05-31 08:07:29 +0300

    работа со стр orders

--- src/frontend/src/store/modules/orders.store.js
+++ src/frontend/src/store/modules/orders.store.js
@@ -1,12 +1,22 @@
 export default {
   namespaced: true,
-  state: {},
+  state: {
+    orders: [],
+  },
   getters: {},
   mutations: {},
   actions: {
     // eslint-disable-next-line no-unused-vars
     async postOrder({ state }, postData) {
       await this.$api.orders.post(postData);
+    },
+    async getOrders({ state }) {
+      const orders = await this.$api.orders.query();
+      state.orders.push(...orders);
+    },
+    async deleteOrder({ dispatch }, orderId) {
+      await this.$api.orders.delete(orderId);
+      dispatch("getOrders");
     },
   },
 };
